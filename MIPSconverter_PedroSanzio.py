e971fd91c161b29a94800a80edccebcfc2a02c5d
Pedro Sanzio <72276805+phsanzio@users.noreply.github.com>
2024-07-29 21:22:44 -0300

    Add files via upload

--- MIPSconverter_PedroSanzio.py
+++ MIPSconverter_PedroSanzio.py
@@ -26,8 +26,9 @@ data_functions_r = {
 }
 
 data_registers = {
-    '$zero': '00000', '$t0': '01000', '$t1': '01001', '$t2': '01010',
-    '$t3': '01011', '$t4': '01100', '$t5': '01101', '$t6': '01110', '$t7': '01111', '$s0': '10000', '$s1': '10001', '$s2': '10010', '$s3': '10011', '$s4': '10100', '$s5': '10101', '$s6': '10110', '$s7': '10111'
+    '$zero': '00000', '$r0': '00000', '$at': '00001','$v0': '00010', '$v1': '00011', '$a0': '00100', '$a1': '00101', '$a2': '00110', '$a3': '00111',
+    '$t0': '01000', '$t1': '01001', '$t2': '01010', '$t3': '01011', '$t4': '01100', '$t5': '01101', '$t6': '01110', '$t7': '01111', '$s0': '10000', '$s1': '10001', '$s2': '10010', '$s3': '10011', '$s4': '10100', '$s5': '10101', '$s6': '10110', '$s7': '10111', '$t8': '11000', '$t9': '11001',
+    '$k0': '11010', '$k1': '11011', '$gp': '11100', '$sp': '11101', '$s8': '11110', '$ra': '11111', 'HI': 'HI', 'LO': 'LO', 'PC': 'PC'
 }
 
 def convertion(instruction):
@@ -192,6 +193,6 @@ def text_generator(read_file, write_file):
 
 username = str(input('Digite seu nome: '))
 filename = str(input('Nome do arquivo para os resultados: '))
-read_file = 'arquivo_teste2.txt'
+read_file = 'arquivo_teste3.txt'
 write_file = str(filename) + '.txt'
 text_generator(read_file, write_file)
